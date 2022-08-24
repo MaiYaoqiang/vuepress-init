7b7c2dcf6c63209aee040d7bc099b191fb4e1d76
MaiYaoqiang <MaiYaoqiang@users.noreply.github.com>
2022-08-24 08:54:48 +0000

    deploy: 5ec99cacd203f7d489d3b8cbcb0445ee9200f574

---
+++ assets/index.html.9f39c4d5.js
@@ -0,0 +1,5 @@
+import{_ as e,o as i,c as n,a as t}from"../app.0f4b39f4.mjs";const a={},s=t(`<h3 id="\u7528\u6CD5" tabindex="-1"><a class="header-anchor" href="#\u7528\u6CD5" aria-hidden="true">#</a> \u7528\u6CD5</h3><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>git clone https://github.com/MaiYaoqiang/vuepress-init.git
+git remote set-url origin &#39;\u81EA\u5DF1\u7684\u5730\u5740&#39;
+npm install
+npm run dev
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>\u7136\u540E\u5F00\u59CB\u6109\u5FEB\u7684\u5728pageList\u6587\u4EF6\u5939\u4E0B\u9762\u5EFA\u8BF4\u660E\u6587\u4EF6</p>`,3),d=[s];function r(c,l){return i(),n("div",null,d)}const u=e(a,[["render",r],["__file","index.html.vue"]]);export{u as default};
